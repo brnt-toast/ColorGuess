bd4fcc6342f78bca789e39a487ab8553c36677a1
brnt-toast <yoyo.beerme@gmail.com>
2023-03-23 17:41:35 -0400

    App.js: refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React from 'react'
 
+function Hero () {
+
+    return (
+        <div className='Hero 
+            bg-slate-300 text-center'>
+                the great guessing game
+        </div>
+    )
+
+}
+
+function Board() {
+
+    const Square = () => <div className='bg-red-600' />
+
+
+    return (
+       <div className={`grid gap-4 grid-cols-2 grid-rows-2 bg-blue-400`} 
+            style={{height: window.innerWidth}}
+        >
+         <Square />
+         <Square />
+         <Square />
+         <Square />
+            
+       </div>
+    )
+
+}
+
 export default App = () => {
 
     return(
         <div className="App">
-            <h1>Hello World </h1>
+            <Hero />
+            <Board />
         </div>
     ) 
 }
